c6eecac56e7e8a8c496cfbb83a5d3b771f62af40
prapti-esther <jprapti52@gmail.com>
2025-03-30 17:02:45 +0530

    signinupui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Mona_Sans} from "next/font/google";
 import "./globals.css";
+import {Toaster} from "sonner";
+import React from "react";
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
@@ -10,7 +12,7 @@ const monaSans = Mona_Sans({
 
 
 export const metadata: Metadata = {
-  title: "PrepWise",
+  title: "ShineBright",
   description: "An AI powered platform for ai mock interview",
 };
 
@@ -21,10 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body
-        className={`${monaSans.className} antialiased`}
-      >
-        {children}
+      <body className={`${monaSans.className} antialiased pattern`}>
+
+      {children}
+      <Toaster />
       </body>
     </html>
   );
---
+++ components/FormFIeld.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {Control, Controller, FieldValues, Path} from "react-hook-form";
+import {T} from "tailwindcss/dist/types-B254mqw1";
+
+interface FormFieldProps<T extends FieldValues>{
+ control: Control <T>;
+ name: Path<T>;
+ label :string;
+ placeholder?: string;
+ type?: 'text' |'email'| 'password'|'file'
+
+}
+
+
+
+const FormFIeld = ({control, name, label, placeholder,type ="text"} : FormFieldProps<T>) => (
+   <Controller
+       name={name}
+       control={control}
+       render={({field})  =>(
+
+       <FormItem>
+           <FormLabel className="label">{label}</FormLabel>
+           <FormControl>
+               <Input  className="input"
+                       placeholder= {placeholder}
+                       type={type}
+                       {...field} />
+           </FormControl>
+
+           <FormMessage />
+       </FormItem>
+   )}
+
+
+    />
+);
+export default FormFIeld
+ 
